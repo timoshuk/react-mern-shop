073241bad7632f8c5c4fb0a7da403e906f93d7da
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-12-08 14:00:05 +0200

    Add react-bootstarp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/header"
+import Footer from "./components/footer"
 
 const App = () => {
   return (
     <>
-      <h1>Welkome to ReactShop</h1>
+
+      <Header />
+      <main>
+        <Container>
+          <h1>Welkome to ReactShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
