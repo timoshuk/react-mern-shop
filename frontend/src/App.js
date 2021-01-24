a4a7004cb3ac4586e449083e7cea5f7f6eac789d
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-01-24 19:35:06 +0200

    3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import Header from "./components/header"
-import Footer from "./components/footer"
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
     <>
-
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welkome to ReactShop</h1>
         </Container>
@@ -16,6 +15,6 @@ const App = () => {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
