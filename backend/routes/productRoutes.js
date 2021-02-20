8d52493b9f91af64a99e1154c677efb29631969b
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-02-20 13:32:13 +0200

    5.1

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -27,7 +27,8 @@ router.get(
     if (products) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404);
+      throw new Error("Product not found");
     }
   })
 );
